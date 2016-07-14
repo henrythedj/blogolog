0cb4d37fcb40affd429a07ccd9b0439c63e4f6e8
Henry Harding McCall <hardingmccall@gmail.com>
2016-07-14 10:42:53 -0500

    form-based workflow completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -17,11 +17,26 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.save
+		flash.notice = "New post created, yahhh"
+		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Post '#{@article.title}' has be updated. Hope it's not crap this time"
+
 		redirect_to article_path(@article)
 	end
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
+
+		flash.notice = "Post '#{@article.title}' has been burninated"
 		redirect_to articles_path
 	end
 end
